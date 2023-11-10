24d285f2dea502e5acc338d939fc183fbe02a945
Francisco <franciscopere987@gmail.com>
2023-11-10 12:08:55 -0300

    Leader election nearly finished

--- invitation/election.go
+++ invitation/election.go
@@ -6,8 +6,8 @@ import (
 )
 
 /*
-	Runs the election, sends only Invites and recieves only Invites
-	Anything else is discarded
+Runs the election, sends only Invites and recieves only Invites
+Anything else is discarded
 */
 func (st *Status) runElection(at io.ReadWriter) (nextStage uint, err error) {
 	backoff := utils.BackoffFrom(int(st.id))
@@ -17,12 +17,12 @@ func (st *Status) runElection(at io.ReadWriter) (nextStage uint, err error) {
 	for err == nil && (st.leaderId == st.id || len(st.members) != len(st.peers)) {
 		stream, readed := backoff.BackoffOnFailure(channel)
 		if readed {
-			st.checkInvitation(stream)
-		}else{
+			st.checkInvitation(stream, at)
+		} else {
 			err = st.invitePeer(missing, at)
 		}
 	}
-	
+
 	if st.leaderId == st.id && err == nil {
 		nextStage = Coordinator
 	}
@@ -30,15 +30,55 @@ func (st *Status) runElection(at io.ReadWriter) (nextStage uint, err error) {
 	return
 }
 
-func (st *Status) checkInvitation(stream []byte) {
+func (st *Status) checkInvitation(stream []byte, at io.ReadWriter) error {
 	if stream[0] == Invite {
-		//Parse invitation
-		//Check if group size is greater than mine
+		inv, err := deserializeInv(stream[1:])
+		if err != nil {
+			return err
+		}
+		if inv.GroupSize > uint(len(st.members)) {
+			st.leaderId = inv.Id
+			_, err := writeToWithRetry(accept{
+				GroupSize: uint(len(st.members)),
+				Members:   st.members,
+			}, at, st.getPeer(inv.Id))
+			return err
+		}
 	}
+
+	return nil
+}
+
+func (st Status) getPeer(peerId uint) string {
+	return ""
+}
+
+func (st *Status) invitationResponse(response []byte) error {
+	//Checks the response to an invitation message
+	switch response[0] {
+	case Accept:
+		//Add the and its group to my members list
+	case Reject:
+		//Check the id, if 0, the same as the last peer or different
+	default:
+		//mark the client as dead
+	}
+	return nil
 }
 
 func (st *Status) invitePeer(choser *utils.Choser, at io.ReadWriter) error {
 	//Send invitation to peer, rejecting every other invitation with id = 0
 	//If peer rejects, and the id is diferent from 0, then send accept and change the leader id
-	return nil
+	peer := choser.Choose()
+	inv := invite{
+		Id:        st.id,
+		GroupSize: uint(len(st.members)),
+	}
+	response, err := writeToWithRetry(inv, at, st.getPeer(peer))
+
+	if err == nil {
+		err = st.invitationResponse(response)
+	}
+
+	return err
 }
--- invitation/invitation.go
+++ invitation/invitation.go
@@ -1,43 +1,69 @@
 package invitation
 
+import (
+	"io"
+)
+
 const (
-	Electing = iota
+	Electing uint = iota
 	Coordinator
 	Member
 )
 
 const (
 	Invite = iota
-	Reject 
+	Reject
 	Accept
 	Change
 	Heartbeat
 	Ok
 )
 
+type invite struct {
+	Id        uint
+	GroupSize uint
+}
+
+type reject struct {
+	LeaderId uint
+}
+
+type accept struct {
+	GroupSize uint
+	Members   []uint
+}
+
+type change struct {
+	NewLeaderId uint
+}
+
+type heartbeat struct{}
+
+type ok struct{}
+
 type Status struct {
-	peers []uint
+	peers   []uint
 	members []uint
 
-	id uint
+	id       uint
 	leaderId uint
 }
 
 func Invitation(peers []uint, id uint) *Status {
 	return &Status{
-		peers: peers,
-		members: make([]uint, 0),
-		id: id,
+		peers:    peers,
+		members:  make([]uint, 0),
+		id:       id,
 		leaderId: id,
 	}
 }
 
-func (st *Status) Run() error {
+func (st *Status) Run(at io.ReadWriter) (err error) {
 	state := Electing
-	for {
-		switch state{
+	for err == nil {
+		switch state {
 		case Electing:
-			//Running an election
+			state, err = st.runElection(at)
 		case Coordinator:
 			//Leader
 		case Member:
@@ -45,5 +71,23 @@ func (st *Status) Run() error {
 		}
 	}
 
+	return
+}
+
+func writeTo(s serializable, at io.Writer, where string) error {
+	//Writes the whole stream into at, if failed to do so, returns an error
 	return nil
+}
+
+func writeToWithRetry(s serializable, at io.ReadWriter, where string) ([]byte, error) {
+	//Tries to pass a stream to the other endpoint and awaits a response
+	//It tries three times, else, an error is returned
+	//If the response is not from the peer i sent the message, sends a reject 0
+	return nil, nil
+}
+
+func readFrom(from io.Reader) ([]byte, error) {
+	//Tries reading from io.Reader
+	//Returns the whole array of bytes readed if successful.
+	return nil, nil
 }
